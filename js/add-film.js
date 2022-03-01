7a62c926abdfee1bf279e56520cf3681f1da8c9d
Oliver Lonsdale <lonsdale1086@outlook.com>
2022-03-01 14:29:51 +0000

    fixed some stuff, probably broken some stuff

--- js/add-film.js
+++ js/add-film.js
@@ -22,77 +22,68 @@ favourite.addEventListener('click', ev => {
   }
 });
 
-//hide and show elements based on the status selected.
-//"status" seems to be a reserved word, so had to use statusA
-//inelegent method, doesn't move stuff below up the page
-statusA.addEventListener('input', ev => {
-  const select = document.getElementById("statusA");
-  const value = select.options[select.selectedIndex].value;
-
-  if (value == "planning") {
-    document.documentElement.style.setProperty('--hiddenTF', "hidden");
-  } else {
-    document.documentElement.style.setProperty('--hiddenTF', "visible");
-  }
+search.addEventListener('click', ev => {
+  alert("not yet implemented")
 });
 
-submit.addEventListener('click', ev => {
+//hide and show elements based on the status selected.
+watched.addEventListener('change', ev => {
+    document.documentElement.style.setProperty('--hiddenTF', "default");  
+});
+
+planning.addEventListener('change', ev => {
+  document.documentElement.style.setProperty('--hiddenTF', "none");
+});
+
+addFilm.addEventListener('submit', ev => {
   saveFilm();
 });
 
 //todo: add more validation
 function saveFilm() {
 
-  //create the files if they don't exist, just stores empty array
-  if (localStorage.getItem("planningFilms") === undefined || localStorage.getItem("planningFilms") === null) {
-    localStorage.setItem("planningFilms", JSON.stringify([]));
-    console.log("Created planningFilms file");
-  }
-  if (localStorage.getItem("watchedFilms") === undefined || localStorage.getItem("watchedFilms") === null) {
-    localStorage.setItem("watchedFilms", JSON.stringify([]));
-    console.log("Created watchedFilms file");
-  }
-
   //get data from form
   const title = document.getElementById("title").value;
   const year = document.getElementById("year").value;
   const runtime = document.getElementById("runtime").value;
-  const status = statusA.options[statusA.selectedIndex].value;
-  const favourite = document.getElementById("favourite").checked;
-  const score = document.getElementById("score").value;
-  const date = document.getElementById("watched-date").value;
-  const watchedNum = document.getElementById("times-watched").value;
+  const status = document.querySelector('input[name="statusSelect"]:checked').value
 
+  let arrayName;
+  let newFilm;
   //save data in correct file
-  //Lots of redunancy. TODO.
-  //saves as array. Object would be better, for dot notation, eg. get film.title rather than film[0]
   if (status == "planning") {
-    const newFilm = [title, year, runtime, status];
-    let planningFilms = JSON.parse(localStorage.getItem("planningFilms"));
-    let dupe = false;
-    planningFilms.forEach(film => {
-      if(film[0]==newFilm[0] && film[1]==newFilm[1]){
-        dupe=true;
-      }
-    });
-    if(dupe == false){
-      planningFilms.push(newFilm)
-      localStorage.setItem("planningFilms", JSON.stringify(planningFilms));
-    }
+    //add data to new object
+    newFilm = {title:title,year:year,runtime:runtime,status:status};
+    //select correct localStorage stored array
+    arrayName = "planningFilms"
   //watched  
-  } else{
-    const newFilm = [title, year, runtime, status, favourite, score, date, watchedNum];
-    let watchedFilms = JSON.parse(localStorage.getItem("watchedFilms"));
-    let dupe = false;
-    watchedFilms.forEach(film => {
-      if(film[0]==newFilm[0] && film[1]==newFilm[1]){
-        dupe=true;
-      }
-    })
-    if(dupe == false){
-      watchedFilms.push(newFilm)
-      localStorage.setItem("watchedFilms", JSON.stringify(watchedFilms));
-    }
+  } else {
+    //collect the rest of the data
+    const favourite = document.getElementById("favourite").checked;
+    const score = document.getElementById("score").value;
+    const date = document.getElementById("watchedDate").value;
+    const watchedNum = document.getElementById("timesWatched").value;
+    //add to new object
+    newFilm = {title:title,year:year,runtime:runtime,status:status,
+      favourite:favourite,score:score,date:date,timesWatched:watchedNum};
+    //select correct localStorage stored array
+    arrayName = "watchedFilms"
   }
-
+  //get correct array from localStorage
+  let array = JSON.parse(localStorage.getItem(arrayName));
+  //if not dupe, push. Broken.
+  if(array.includes(newFilm) == false){
+    array.push(newFilm);
+    localStorage.setItem(arrayName, JSON.stringify(array));
+  }
 };
+
+//create the files if they don't exist, just stores empty array
+if (localStorage.getItem("planningFilms") === undefined || localStorage.getItem("planningFilms") === null) {
+  localStorage.setItem("planningFilms", JSON.stringify([]));
+  console.log("Created planningFilms file");
+}
+if (localStorage.getItem("watchedFilms") === undefined || localStorage.getItem("watchedFilms") === null) {
+  localStorage.setItem("watchedFilms", JSON.stringify([]));
+  console.log("Created watchedFilms file");
+}
